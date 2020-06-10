2a8efe087d61b691c59cd9a93dc04385a6dd978a
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-06-10 18:29:16 +0200

    Clear pause menu

---
+++ Þurisaz! Thor_s mighty rescue adventure/Assets/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuPanel;
+    public GameObject SettingsMenu;
+    public GameObject ControlMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void SettingsPause()
+    {
+        PauseMenuPanel.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+    public void ControlsPause()
+    {
+        PauseMenuPanel.SetActive(false);
+        ControlMenu.SetActive(true);
+    }
+    public void ContainerPause()
+    {
+        SettingsMenu.SetActive(false);
+        ControlMenu.SetActive(false);
+        PauseMenuPanel.SetActive(true);
+    }
+
+
+}
