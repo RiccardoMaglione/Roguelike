d8b6e6dd9643555e016820e9119f681fdd17ed27
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-06-16 00:00:24 +0200

    Start gamepad menu

---
+++ Þurisaz! Thor_s mighty rescue adventure/Assets/GamepadButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GamepadButton : MonoBehaviour
+{
+    public GameObject PlayFirstButton;
+    public GameObject MonsterpediaFirstButton;
+    public GameObject SettingFirstButton;
+    public GameObject CreditsFirstButton;
+    public GameObject ExitFirstButton;
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(PlayFirstButton);
+    }
+
+}
