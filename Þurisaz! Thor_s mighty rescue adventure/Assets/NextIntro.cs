cce20ab1fd3f03a92e72b86228f188ae209c8deb
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-06-19 17:13:01 +0200

    Add intro and decoration down door

---
+++ Þurisaz! Thor_s mighty rescue adventure/Assets/NextIntro.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextIntro : MonoBehaviour
+{
+    public GameObject Image1;
+    public GameObject Image2;
+    public GameObject Image3;
+    int press = 0;
+    bool once;
+
+    public void Next()
+    {
+        if(once == true)
+        {
+            press += 1;
+            once = false;
+        }
+        if (press == 0)
+        {
+            Debug.Log("Schiaccia");
+            Image1.SetActive(false);
+            Image2.SetActive(true);
+            once = true;
+        }
+        if (press == 1)
+        {
+            Image2.SetActive(false);
+            Image3.SetActive(true);
+            once = true;
+        }
+        if (press == 2)
+        {
+            Image3.SetActive(false);
+            SceneManager.LoadScene("StartingScreen");
+        }
+    }
+}
